22a7769411e929147640cd4ba08e80ad94eb4622
Jiniyas Shah <72019447+jiniyasshah@users.noreply.github.com>
2024-05-03 20:21:48 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "sanks",
-    project: "javascript-react"
-  })],
-
-  build: {
-    sourcemap: true
-  }
-})
+  plugins: [react()],
+});
